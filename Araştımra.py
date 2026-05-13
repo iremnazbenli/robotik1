f90afe2c11bc06b9c6de6b20b7fa389a301362cc
iremnazbenli <iremnazbenli@gmail.com>
2026-05-13 09:39:52 +0300

    Create Araştımra.py

---
+++ Araştımra.py
@@ -0,0 +1,8 @@
+Integer (Tam Sayılar - int)
+Matematikteki tam sayıları temsil eder. Ondalık kısım içermez; pozitif, negatif veya sıfır olabilir.
+• Kullanımı: Sayma işlemleri, adet belirtme veya matematiksel hesaplamalar için kullanılır.
+• Örnek: x = 5, y = -10
+2. Float (Ondalıklı Sayılar - float)
+Ondalık noktası olan sayıları ifade eder. Hassas ölçümler ve bölme işlemleri için idealdir.
+• Kullanımı: Fiyat hesaplamaları, ağırlık ölçümleri veya koordinatlar için kullanılır.
+• Örnek: pi = 3.14, boy = 1.75
